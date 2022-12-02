a214be1c322d7ad97930e5a64c69d6215a0818a3
Pablo Valdez <95011285+Patriciobe@users.noreply.github.com>
2022-12-02 00:20:39 -0500

    Wey
    Otrowey

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert("1. Eres lo más valioso para nosotros. ¡Esperamos contar contigo durante muchos Años más! Feliz Navidad y prosperidad para todos Dios me los bendiga siempre ");
+alert("1. Eres lo más valioso para nosotros. ¡Esperamos contar contigo durante muchos Años más! Feliz Navidad y próspero año nuevo");
